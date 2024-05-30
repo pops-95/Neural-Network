70c6c79edeed5a5d2f2606766b46447fe41c7866
pops-95 <layekfdi@gmail.com>
2024-05-30 18:01:18 +0530

    layer constructur

--- include/layer.hpp
+++ include/layer.hpp
@@ -6,7 +6,15 @@ class layer{
 		std::vector<int> hidden_layer_nodes;
 		
 	public:
+		layer(int number,std::vector<int> nodes_num);
 		
 };
+
+layer::layer(int number,std::vector<int> nodes_num){
+	__number_hidden_layer=number;
+	hidden_layer_nodes=nodes_num;
+}
+
+
 
 
