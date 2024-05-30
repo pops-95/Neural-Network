05c08e7f6c9498d234ce1ccdf46f64c431957933
pops-95 <layekfdi@gmail.com>
2024-05-30 16:57:30 +0530

    changes wirght init

--- include/hidden_layer.hpp
+++ include/hidden_layer.hpp
@@ -10,12 +10,14 @@ private:
         int __input_nodes;
         std::string activation_fun;
         std::vector<int> __weights;
+        void init_weights();
                 
 public:
     hidden_layer(int nodes,std::string activation);
     ~hidden_layer();
     int get_layer_number();
-    int set_input_nodes(int value);
+    void set_input_nodes(int value);
+    void show_weights();
 };
 
 hidden_layer::hidden_layer(int nodes,std::string activation)
@@ -32,8 +34,30 @@ int hidden_layer::get_layer_number(){
     return __number_of_node;
 }
 
-int hidden_layer::set_input_nodes(int values){
+void hidden_layer::set_input_nodes(int values){
     __input_nodes=values;
+    init_weights();
 }
+
+void hidden_layer::show_weights()
+{
+    for(int i: __weights){
+        std::cout<<i<<"\n";
+    }
+}
+
+void hidden_layer::init_weights(){
+    if(__input_nodes!=0){
+        for (int i=0;i<(__input_nodes*__number_of_node);i++){
+            __weights.push_back(i);
+        }
+    }
+    else{
+        std::cout<<"input node not defined"<<"\n";
+    }
+    
+}
+
+
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,8 @@
 int main(int argc, char *argv[])
 {
     hidden_layer layer=hidden_layer(2,"value");
-    std::cout << layer.get_layer_number() << std::endl;
+    layer.set_input_nodes(8);
+    layer.show_weights();
+
+    return 0;
 }
