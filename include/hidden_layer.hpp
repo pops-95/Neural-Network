f1559d4c9f6ed06dcdac0ed170231b80ca284918
pops-95 <layekfdi@gmail.com>
2024-05-30 16:46:59 +0530

    changes

--- include/hidden_layer.hpp
+++ include/hidden_layer.hpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 class hidden_layer
 {
 private:
         int __number_of_node;
-        static int __input_nodes;
+        int __input_nodes;
         std::string activation_fun;
-        int __weights[__input_nodes];
+        std::vector<int> __weights;
                 
 public:
     hidden_layer(int nodes,std::string activation);
     ~hidden_layer();
     int get_layer_number();
+    int set_input_nodes(int value);
 };
 
 hidden_layer::hidden_layer(int nodes,std::string activation)
@@ -29,3 +31,9 @@ hidden_layer::~hidden_layer()
 int hidden_layer::get_layer_number(){
     return __number_of_node;
 }
+
+int hidden_layer::set_input_nodes(int values){
+    __input_nodes=values;
+}
+
+
