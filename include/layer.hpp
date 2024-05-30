60bd888c1e86d629c94d032f42889be72e0686bf
pops-95 <layekfdi@gmail.com>
2024-05-30 18:04:13 +0530

    changes

--- include/layer.hpp
+++ include/layer.hpp
@@ -4,16 +4,18 @@ class layer{
 	private:
 		int __number_hidden_layer;
 		std::vector<int> hidden_layer_nodes;
+		std::vector<int> output_values;
 		
 	public:
 		layer(int number,std::vector<int> nodes_num);
-		
+		void calculate
 };
 
 layer::layer(int number,std::vector<int> nodes_num){
 	__number_hidden_layer=number;
 	hidden_layer_nodes=nodes_num;
 }
+
 
 
 
